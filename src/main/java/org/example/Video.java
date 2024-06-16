0753dc703e479653971bde7c83847d982940fbd2
YasharNasiri <bluemustarduniversity@gmail.com>
2024-06-16 16:30:06 +0330

    Configured a type for the video file in the Video class.

--- src/main/java/org/example/Video.java
+++ src/main/java/org/example/Video.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.awt.*;
+import java.sql.Blob;
 import java.util.ArrayList;
 
 public class Video
@@ -10,10 +11,11 @@ public class Video
     String title;
     String description;
     ArrayList<String> commentIDs;                    //the ID of the comments under the video, the order in the array list is better to also follow the order which the comments have been made
-    ArrayList<String> viewerIDs;                       //to store the ID of the users which have viewed the video followed by either 0 for no like or dislike, 1 for like and 2 for dislike (this is prone to change)
+    ArrayList<String> viewerIDs;                     //to store the ID of the users which have viewed the video followed by either 0 for no like or dislike, 1 for like and 2 for dislike (this is prone to change)
     int likes;                                       //to store the number of likes
     int dislike;                                     //to store the number of dislikes
     Image thumbnail;                                 //this is a placeholder and will be replaced by the correct format/variable type
+    Blob videoData;                                  //this variable holds the raw Blob data of the video from database which must then be converted into video to be played or streamed (this is prone to change)
 
     public Video()
     {
