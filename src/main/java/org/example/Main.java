27062bf160482273a2c0323b168f36e0a03eae33
YasharNasiri <bluemustarduniversity@gmail.com>
2024-06-16 16:05:49 +0330

    Just cleaning up

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ public class Main
     static CCM ccm;
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        System.out.println("BANANA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
 }
--- src/main/java/org/example/Video.java
+++ src/main/java/org/example/Video.java
@@ -10,7 +10,7 @@ public class Video
     String title;
     String description;
     ArrayList<String> commentIDs;                    //the ID of the comments under the video, the order in the array list is better to also follow the order which the comments have been made
-    ArrayList<String> viewers;                       //to store the ID of the users which have viewed the video followed by either 0 for no like or dislike, 1 for like and 2 for dislike (this is prone to change)
+    ArrayList<String> viewerIDs;                       //to store the ID of the users which have viewed the video followed by either 0 for no like or dislike, 1 for like and 2 for dislike (this is prone to change)
     int likes;                                       //to store the number of likes
     int dislike;                                     //to store the number of dislikes
     Image thumbnail;                                 //this is a placeholder and will be replaced by the correct format/variable type
