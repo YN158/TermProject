d2e94889f5d92da6f0b644de0b43760f1d7bd3a5
YasharNasiri <bluemustarduniversity@gmail.com>
2024-06-27 11:09:54 +0330

    Implimented the MangoDB database

--- src/main/java/org/example/NetWork/Server.java
+++ src/main/java/org/example/NetWork/Server.java
@@ -3,9 +3,43 @@ package org.example.NetWork;
 import java.io.*;
 import java.net.*;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 public class Server implements Runnable {
     public static final int port = 9090;
 
+    //mongodb password: 691NfDk7mwCNeTJt
+    //mangodb username: bluemustardsauce
+
+    public static void main(String[] args) {
+        String connectionString = "mongodb+srv://bluemustardsauce:691NfDk7mwCNeTJt@termproject.xdwhar0.mongodb.net/?appName=TermProject";
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("admin");
+                database.runCommand(new Document("ping", 1));
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     public void run() {
         try{
