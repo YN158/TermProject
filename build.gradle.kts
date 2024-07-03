c6416057437e6d6059e6fc56d6f9937d35798a15
YasharNasiri <bluemustarduniversity@gmail.com>
2024-07-03 19:37:47 +0330

    well I think it worked? VLCJ lezz go???

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,9 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.openjfx:javafx-controls:19")
     testImplementation("org.openjfx:javafx-fxml:19")
+    implementation("javax.media:jmf:2.1.1e")
+    testImplementation("com.sun.media:jmf:2.1.1e")
+    implementation("uk.co.caprica:vlcj:4.8.3")
 }
 
 tasks.test {
--- src/main/java/org/example/panels/VideoPanel.java
+++ src/main/java/org/example/panels/VideoPanel.java
@@ -5,8 +5,8 @@ import org.example.GUI;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowEvent;
-//import javax.media.Manager;
-//import javax.media.Player;
+import javax.media.Manager;
+import javax.media.Player;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
@@ -19,17 +19,30 @@ public class VideoPanel extends JPanel
         setLayout(null);
 
         /*try {
-            // Create the player
-            Player player = Manager.createRealizedPlayer("path/to/your/video.mp4");
+            Manager.setHint(Manager.LIGHTWEIGHT_RENDERER, true);
+            Player mediaPlayer = Manager.createRealizedPlayer(mediaURL);
+            Component video = mediaPlayer.getVisualComponent();
+            Component controls = mediaPlayer.getControlPanelComponent();
 
-            // Add the player to the content pane
-            getContentPane().add(player.getVisualComponent(), "Center");
-            getContentPane().add(player.getControlPanelComponent(), "South");
-        } catch (Exception e) {
-            e.printStackTrace();
+            if (video != null)
+
+                add(video, BorderLayout.CENTER);
+
+            if (controls != null)
+                add(controls, BorderLayout.SOUTH);
+
+            mediaPlayer.start();
+        } catch (NoPlayerException noPlayerException) {
+            System.err.println("No media player found");
+        } // end catch
+        catch (CannotRealizeException cannotRealizeException) {
+            System.err.println("Could not realize media player");
+        } // end catch
+        catch (IOException iOException) {
+            System.err.println("Error reading from the source");
         }*/
 
-        setVisible(false);
+        setVisible(true);
         setSize(1550, 838);
     }
 
