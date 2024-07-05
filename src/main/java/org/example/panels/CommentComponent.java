c7b122436401e9c97695cb5e5dc8354a52b66e85
YasharNasiri <bluemustarduniversity@gmail.com>
2024-07-05 11:29:55 +0330

    Created the CommentComponent for... comments... duh

---
+++ src/main/java/org/example/panels/CommentComponent.java
@@ -0,0 +1,20 @@
+package org.example.panels;
+
+import org.example.GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CommentComponent extends JPanel
+{
+    JButton viewAuthorChannel;
+    JLabel commentBody;
+
+    public CommentComponent(String CommentID)
+    {
+        setLayout(new BorderLayout());
+
+
+    }
+
+}
