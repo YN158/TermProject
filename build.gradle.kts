448df79e45455f1e6645f58d38f6ea50bfbe67a4
YasharNasiri <bluemustarduniversity@gmail.com>
2024-07-05 19:17:37 +0330

    added the dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,8 @@ dependencies {
     implementation("javax.media:jmf:2.1.1e")
     testImplementation("com.sun.media:jmf:2.1.1e")
     implementation("uk.co.caprica:vlcj:4.8.3")
+    implementation("org.mongodb:mongodb-driver-sync:5.1.1")
+
 }
 
 tasks.test {
--- src/main/java/org/example/NetWork/MongoDb.java
+++ src/main/java/org/example/NetWork/MongoDb.java
@@ -8,18 +8,21 @@ import org.example.Comment;
 import org.example.User;
 import org.example.Video;
 
-public class MongoDb {
+public class MongoDb
+{
     public static MongoClient mongoClient;
-    //    public static DB database;
-//    public static DBCollection test;
-    public static void main(String[] args) {
+    //public static DB database
+    //public static DBCollection test;
+    public static void main(String[] args)
+    {
         mongoClient = new MongoClient(new MongoClient("mongodb+srv://bluemustardsauce:691NfDk7mwCNeTJt@termproject.xdwhar0.mongodb.net/?retryWrites=true&w=majority&appName=TermProject"));
         database = mongoClient.getdb("Mongodb");
         test = database.getCollection("test");
         DBObject obj = new BasicDBOject();
 
     }
-    public static BasicDBObject convert (User user){
+    public static BasicDBObject convert (User user)
+    {
         return new BasicDBObject("ID" , user.getID())
                 .append("email" , user.getEmail())
                 .append("password" , user.getPassword())
@@ -27,7 +30,8 @@ public class MongoDb {
                 .append("subscriptionIDs" , user.getsubscriptionIDs())
                 .append("subscriberIDs" , user.getsubscriberIDs()).append("icon" , user.getIcon());
     }
-    public static BasicDBObject convert (Video video){
+    public static BasicDBObject convert (Video video)
+    {
         return new BasicDBObject("ID" , video.getID())
                 .append("channelId" , video.getChannelID())
                 .append("title" , video.getTitle())
@@ -36,7 +40,8 @@ public class MongoDb {
                 .append("viewerIDs" , video.getViewerIDs()).append("likes" , video.getLikes())
                 .append("dislikes" , video.getDislike()).append("thumbnail" , video.getThumbnail());
     }
-    public static BasicDBObject convert (Comment comment){
+    public static BasicDBObject convert (Comment comment)
+    {
         return new BasicDBObject("ID" , comment.getID())
                 .append("videoID" , comment.getVideoID())
                 .append("body" , comment.getBody())
