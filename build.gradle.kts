672bbdfbe6d9caa504e5aba7c677e415a8e263e7
YasharNasiri <bluemustarduniversity@gmail.com>
2024-07-05 19:02:04 +0330

    updated the Build_gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,11 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation ("org.mongodb:mongodb-driver-sync:5.1.1")
+    testImplementation("org.openjfx:javafx-controls:19")
+    testImplementation("org.openjfx:javafx-fxml:19")
+    implementation("javax.media:jmf:2.1.1e")
+    testImplementation("com.sun.media:jmf:2.1.1e")
+    implementation("uk.co.caprica:vlcj:4.8.3")
 }
 
 tasks.test {
