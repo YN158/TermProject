2f9cec5c3b72f4b8f9d92bf72463b945c2984243
YasharNasiri <bluemustarduniversity@gmail.com>
2024-07-09 18:31:11 +0330

    Trying to configure the DB

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,10 @@ dependencies {
     implementation("javax.media:jmf:2.1.1e")
     testImplementation("com.sun.media:jmf:2.1.1e")
     implementation("uk.co.caprica:vlcj:4.8.3")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation ("org.mongodb:mongodb-driver-sync:4.4.2")
+    implementation ("org.mongodb:mongodb-driver-sync:5.1.1")
 }
 
 tasks.test {
--- src/main/java/org/example/NetWork/MongoDb.java
+++ src/main/java/org/example/NetWork/MongoDb.java
@@ -1,54 +1,94 @@
 package org.example.NetWork;
 
-//import com.mongodb.BasicDBObject;
-//import com.mongodb.DBObject;
-//import com.mongodb.client.MongoCollection;
-//import com.mongodb.client.MongoClient;
-//import com.mongodb.client.MongoDatabase;
-//import org.example.Comment;
-//import org.example.User;
-//import org.example.Video;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.*;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import org.bson.Document;
 
-import javax.swing.text.Document;
+public class MongoDb {
 
-public class MongoDb
-{
-//    public static MongoClient mongoClient;
-//    public static MongoDatabase database;
-//    public static MongoCollection<Document> test;
-//    public static void main(String[] args)
-//    {
-//        //mongoClient = new MongoClient(new MongoClient("mongodb+srv://bluemustardsauce:691NfDk7mwCNeTJt@termproject.xdwhar0.mongodb.net/?retryWrites=true&w=majority&appName=TermProject"));
-//        database = mongoClient.getDatabase("Mongodb");
-//        //test = database.getCollection();
-//        //DBObject obj = new BasicDBOject();
-//    }
-//    public static BasicDBObject convert (User user)
-//    {
-//        return new BasicDBObject("ID" , user.getID())
-//                .append("email" , user.getEmail())
-//                .append("password" , user.getPassword())
-//                .append("videoIDs" , user.getvideoIDs())
-//                .append("subscriptionIDs" , user.getsubscriptionIDs())
-//                .append("subscriberIDs" , user.getsubscriberIDs()).append("icon" , user.getIcon());
-//    }
-//    public static BasicDBObject convert (Video video)
-//    {
-//        return new BasicDBObject("ID" , video.getID())
-//                .append("channelId" , video.getChannelID())
-//                .append("title" , video.getTitle())
-//                .append("describtion" , video.getDescription())
-//                .append("commentIDs" , video.getCommentIDs())
-//                .append("viewerIDs" , video.getViewerIDs()).append("likes" , video.getLikes())
-//                .append("dislikes" , video.getDislike()).append("thumbnail" , video.getThumbnail());
-//    }
-//    public static BasicDBObject convert (Comment comment)
-//    {
-//        return new BasicDBObject("ID" , comment.getID())
-//                .append("videoID" , comment.getVideoID())
-//                .append("body" , comment.getBody())
-//                .append("userID" , comment.getUserID())
-//                .append("date" , comment.getDate());
-//
-//    }
+    private static MongoClient mongoClient;
+    private static MongoDatabase database;
+
+    public static void connect(String connectionString)
+    {
+        MongoClient mongoClient = MongoClients.create(connectionString);
+        database = mongoClient.getDatabase("youtube");
+    }
+
+    public static void createCollections() {
+        database.createCollection("users");
+        database.createCollection("videos");
+        database.createCollection("comments");
+    }
+
+    // Function to update a user document in the users collection
+    public static void updateUser(String userId, String fieldName, String newValue) {
+        MongoCollection<Document> usersCollection = database.getCollection("users");
+        usersCollection.updateOne(
+                Filters.eq("ID", userId),
+                Updates.set(fieldName, newValue)
+        );
+    }
+
+    // Function to update a video document in the videos collection
+    public static void updateVideo(String videoId, String fieldName, String newValue) {
+        MongoCollection<Document> videosCollection = database.getCollection("videos");
+        videosCollection.updateOne(
+                Filters.eq("ID", videoId),
+                Updates.set(fieldName, newValue)
+        );
+    }
+
+    // Function to update a comment document in the comments collection
+    public static void updateComment(String commentId, String fieldName, String newValue) {
+        MongoCollection<Document> commentsCollection = database.getCollection("comments");
+        commentsCollection.updateOne(
+                Filters.eq("ID", commentId),
+                Updates.set(fieldName, newValue)
+        );
+    }
+    // Function to retrieve and print all documents from the users collection
+    public static void getAllUsers() {
+        MongoCollection<Document> usersCollection = database.getCollection("users");
+        FindIterable<Document> users = usersCollection.find();
+        for (Document user : users) {
+            System.out.println("User: " + user);
+        }
+    }
+
+    // Function to retrieve and print all documents from the videos collection
+    public static void getAllVideos() {
+        MongoCollection<Document> videosCollection = database.getCollection("videos");
+        FindIterable<Document> videos = videosCollection.find();
+        for (Document video : videos) {
+            System.out.println("Video: " + video);
+        }
+    }
+
+    // Function to retrieve and print all documents from the comments collection
+    public static void getAllComments() {
+        MongoCollection<Document> commentsCollection = database.getCollection("comments");
+        FindIterable<Document> comments = commentsCollection.find();
+        for (Document comment : comments) {
+            System.out.println("Comment: " + comment);
+        }
+    }
+
+    public static void close() {
+        if (mongoClient != null) {
+            mongoClient.close();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        String connectionString = "mongodb+srv://bluemustardsauce:<691NfDk7mwCNeTJt>@termproject.xdwhar0.mongodb.net/?retryWrites=true&w=majority&appName=TermProject";
+
+        connect(connectionString);
+        createCollections();
+
+        close();
+    }
 }
