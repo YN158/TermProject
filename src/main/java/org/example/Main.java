042835ee29b7c3391c6fa9691b6a87d12651578c
YasharNasiri <bluemustarduniversity@gmail.com>
2024-07-09 20:52:15 +0330

    trying to initialize a connection to the DB

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,10 @@ public class Main
     public static MongoDBRepository repository;
     public static void main(String[] args)
     {
-        repository = new MongoDBRepository();
+//        repository = new MongoDBRepository();
+//        CCM.users = repository.getAllUsers();
+//        CCM.videos = repository.getAllVideos();
+//        CCM.comments = repository.getAllComments();
         GUI gui = new GUI("TUBE");
     }
 }
